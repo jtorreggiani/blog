743d9cb156b02a04fe5d6d3ff7af6d83056dbf32
Joe Torreggiani <joetorreggiani@gmail.com>
2024-01-09 22:38:01 -0500

    Test using images.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import fs from 'fs'
 import matter from 'gray-matter'
+import Image, { ImageProps } from 'next/image'
+import Link from 'next/link'
 
 
-export default async function Home() {
+export default function Home() {
   const postsDirectory = `${process.cwd()}/app/posts`;
   const filenames = fs.readdirSync(postsDirectory).filter((filename => filename !== 'layout.tsx'));
   const pages = filenames.map((filename) => {
@@ -13,18 +15,35 @@ export default async function Home() {
   });
 
   return (
-    <main className="mx-auto w-1/2">
-      <h1>Joe&apos;s Blog</h1>
+    <main className="mt-12 mx-auto w-3/4">
+      <h1 className="text-4xl">Joe Torreggiani&apos;s Blog</h1>
       <h3 className="text-2xl">Recent posts</h3>
-      <ul>
+      <div>
         {pages.map((page) => (
-          <li key={page.slug}>
-            <a href={`/blog/posts/${page.slug}`}>
-              {page.title}
-            </a>
-          </li>
+          <div className="my-6" key={page.slug}>
+            <div className="grid grid-cols-4 gap-2">
+              <div className="col-span-1 p-0 m-0">
+                <Image
+                  alt={page.title}
+                  src={page.image as ImageProps['src']}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                />
+              </div>
+              <div className="col-span-3 p-0 m-0">
+                <Link className="block text-2xl pt-[1px] m-0 leading-4" href={`/posts/${page.slug}`}>
+                  <span className="block pt-0 m-0">{page.title}</span>
+                </Link>
+                <p className="mt-6 w-full">
+                  {page.description}
+                </p>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </main>
   )
 }
