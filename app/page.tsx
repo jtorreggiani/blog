a89c4762818964d0c41d8153dbd598d194fb8730
Joe Torreggiani <joetorreggiani@gmail.com>
2024-01-09 20:52:02 -0500

    Add listing markdown articles.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
-import Image from 'next/image'
+import fs from 'fs'
+import matter from 'gray-matter'
 
-export default function Home() {
+
+export default async function Home() {
+  const postsDirectory = `${process.cwd()}/app/posts`;
+  const filenames = fs.readdirSync(postsDirectory).filter((filename => filename !== 'layout.tsx'));
+  const pages = filenames.map((filename) => {
+    const path = `${postsDirectory}/${filename}/page.mdx`;
+    let file = fs.readFileSync(path, 'utf8');
+    const { data } = matter(file);
+    return data;
+  });
+
   return (
-    <main className="">
+    <main className="mx-auto w-1/2">
       <h1>Joe's Blog</h1>
+      <h3 className="text-2xl">Recent posts</h3>
+      <ul>
+        {pages.map((page) => (
+          <li>
+            <a href={`/posts/${page.slug}`}>
+              {page.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
